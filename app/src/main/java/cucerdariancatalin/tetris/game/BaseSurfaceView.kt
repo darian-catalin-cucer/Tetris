42adf0347994528f08ade2c46c253e3353f60551
Darian-Cătălin (Dorian) Cucer <cucerdariancatalin@gmail.com>
2023-09-03 11:54:04 +0300

    BaseSurfaceView.kt

--- app/src/main/java/cucerdariancatalin/tetris/game/BaseSurfaceView.kt
+++ app/src/main/java/cucerdariancatalin/tetris/game/BaseSurfaceView.kt
@@ -14,20 +14,23 @@ import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.newSingleThreadContext
 
+// Abstract base class for SurfaceView-based views
 @Suppress("LeakingThis")
 abstract class BaseSurfaceView(context: Context, attrs: AttributeSet? = null) : SurfaceView(context, attrs) {
+    // Callback to handle SurfaceHolder events
     private val holderCallback: SurfaceHolder.Callback = object : SurfaceHolder.Callback {
-        override fun surfaceCreated(holder: SurfaceHolder?) {
+        override fun surfaceCreated(holder: SurfaceHolder) {
             surfaceReady = true
         }
 
-        override fun surfaceDestroyed(holder: SurfaceHolder?) {
+        override fun surfaceDestroyed(holder: SurfaceHolder) {
             surfaceReady = false
         }
 
-        override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {}
+        override fun surfaceChanged(holder: SurfaceHolder, p1: Int, p2: Int, p3: Int) {}
     }
 
+    // Flag to indicate if the surface is ready
     protected var surfaceReady = false
 
     init {
@@ -36,8 +39,11 @@ abstract class BaseSurfaceView(context: Context, attrs: AttributeSet? = null) : 
         holder.addCallback(holderCallback)
     }
 
+    // Coroutine-based actor for rendering on the surface
     @ObsoleteCoroutinesApi
-    private val surfaceActor = MainScope().actor<Unit>(newSingleThreadContext(javaClass.simpleName), 2) {
+    private val surfaceActor = MainScope().actor<Unit>(
+        newSingleThreadContext(javaClass.simpleName), 2
+    ) {
         while (isActive) {
             receive()
             var canvas: Canvas? = null
@@ -57,18 +63,21 @@ abstract class BaseSurfaceView(context: Context, attrs: AttributeSet? = null) : 
         }
     }
 
+    // Invalidate the view and send a rendering request to the actor
     @ObsoleteCoroutinesApi
     override fun invalidate() {
         super.invalidate()
-        if (surfaceReady) surfaceActor.offer(Unit)
+        if (surfaceReady) surfaceActor.trySend(Unit).isSuccess
     }
 
+    // Post-invalidate the view and send a rendering request to the actor
     @ObsoleteCoroutinesApi
     override fun postInvalidate() {
         super.postInvalidate()
-        if (surfaceReady) surfaceActor.offer(Unit)
+        if (surfaceReady) surfaceActor.trySend(Unit).isSuccess
     }
 
+    // Clean up resources when the view is detached from the window
     @ObsoleteCoroutinesApi
     override fun onDetachedFromWindow() {
         super.onDetachedFromWindow()
