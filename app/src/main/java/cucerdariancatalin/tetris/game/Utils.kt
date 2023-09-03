34d602a58b4b003ebbd67c3b58cf8318bb826a22
Darian-Cătălin (Dorian) Cucer <cucerdariancatalin@gmail.com>
2023-09-03 12:05:24 +0300

    Comments + Fixes

--- app/src/main/java/cucerdariancatalin/tetris/game/Utils.kt
+++ app/src/main/java/cucerdariancatalin/tetris/game/Utils.kt
@@ -4,12 +4,31 @@ import android.util.Log
 import java.text.SimpleDateFormat
 import java.util.*
 
+/**
+ * Utility function for logging messages with timestamps.
+ *
+ * @param s The message to be logged.
+ */
 fun log(s: String) = println("${getDate()}: $s")
+
+/**
+ * Get the current timestamp in the "kk:mm:ss.SS" format.
+ *
+ * @return The formatted timestamp.
+ */
 private fun getDate(): String = SimpleDateFormat("kk:mm:ss.SS").format(Date())
 
+/**
+ * A profiling function for measuring the execution time of a code block.
+ *
+ * @param name The name or description of the code block being profiled.
+ * @param block The code block to be executed and profiled.
+ * @return The result of the code block.
+ */
 inline fun <T> profile(name: String, block: () -> T): T {
-    var start = System.currentTimeMillis()
+    val start = System.currentTimeMillis()
     val result = block()
-    Log.i("profile", "$name ---> ${System.currentTimeMillis() - start}ms")
+    val executionTime = System.currentTimeMillis() - start
+    Log.i("profile", "$name ---> $executionTime ms")
     return result
 }
